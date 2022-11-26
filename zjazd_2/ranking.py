53cc99d3bcfc56909a6b22fc5f8d93d969ed4862
myjniak <myjniak@gmail.com>
2022-11-26 17:14:41 +0100

    sorted

--- zjazd_2/ranking.py
+++ zjazd_2/ranking.py
@@ -6,6 +6,9 @@ for linia in linijki_pliku:
     zawodnik, punktacja = linia.split(",")
     ranking[zawodnik] = int(punktacja)
 
-print(json.dumps(ranking, indent=4))
+# print(json.dumps(ranking, indent=4))
 
-print(sorted(ranking))
+s = sorted(ranking.items(), key=lambda x: x[1], reverse=True)
+print(f"Wygrywa {s[0][0]} z {s[0][1]}pkt")
+print(f"2 miejsce: {s[1][0]} z {s[1][1]}pkt")
+print(f"3 miejsce: {s[2][0]} z {s[2][1]}pkt")
