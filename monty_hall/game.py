34cee658c4b5ff6313b80f01d121cb46deaf496e
myjniak <myjniak@gmail.com>
2023-03-04 14:32:58 +0100

    ready for Enum

--- monty_hall/game.py
+++ monty_hall/game.py
@@ -8,28 +8,45 @@ import random
 from . import logger
 
 
+class MontyHallBadDoorError(ValueError):
+    """When the chosen door number does not exist or are already opened"""
+
+
 class Game:
 
     def __init__(self, door_count=3):
         self.door_count = door_count
         self.__winning_door = random.randint(1, door_count)
+        self._doors_for_final_guess = []
+        self._status = "Not started"
 
     def first_guess(self, door: int) -> list[int]:
         doors = list(range(1, self.door_count + 1))
+        if door not in doors:
+            raise MontyHallBadDoorError(f"Door number {door} does not exist. Existing doors: {doors}")
         logger.debug(f"Doors to choose from: {doors}")
         doors.remove(door)
         if door == self.__winning_door:
             remaining_door = random.choice(doors)
             doors.remove(remaining_door)
+            self._doors_for_final_guess = [door, remaining_door]
         else:
             doors.remove(self.__winning_door)
+            self._doors_for_final_guess = [door, self.__winning_door]
         logger.info(f"Zygmunt opens doors with following numbers: {doors} to show there is nothing there!")
+        self._status = "First guess done"
         return doors
 
     def final_guess(self, door: int) -> bool:
+        if door not in self._doors_for_final_guess:
+            raise MontyHallBadDoorError(f"Door {door} doesn't exist or is already open.\n"
+                                        f"Doors to choose from were: {self._doors_for_final_guess}")
         if door == self.__winning_door:
             logger.info("YOU WON")
+            self._status = "Won"
             return True
         else:
             logger.info("YOU LOST")
+            self._status = "Lost"
             return False
+
--- monty_hall_strategy_test.py
+++ monty_hall_strategy_test.py
@@ -8,11 +8,11 @@ file_handler = logging.FileHandler("log.txt")
 logger.addHandler(file_handler)
 
 
-game_count = 1000
+game_count = 10000
 print("Testing 'choose the same door' strategy")
 won_games = 0
 for i in range(game_count):
-    game = Game()
+    game = Game(door_count=100)
     game.first_guess(1)
     game_won = game.final_guess(1)
     won_games += game_won
@@ -23,11 +23,11 @@ print(f"Win rate = {won_games}/{game_count}")
 print("Testing 'choose the other door' strategy")
 won_games = 0
 for i in range(game_count):
-    game = Game()
+    game = Game(door_count=100)
     empty_doors = game.first_guess(1)
+    empty_doors.append(1)
     doors = list(range(1, game.door_count + 1))
-    doors.remove(1)
-    # doors.remove(empty_door)  usuń empty_doors z doors ????
-    game_won = game.final_guess(doors[0])
+    final_door = (set(doors) - set(empty_doors)).pop()
+    game_won = game.final_guess(final_door)
     won_games += game_won
 print(f"Win rate = {won_games}/{game_count}")
