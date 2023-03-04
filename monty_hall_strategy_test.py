8523b133a0db718a3d0faf55b024f06b7b48a75f
myjniak <myjniak@gmail.com>
2023-03-04 12:59:01 +0100

    best strategy

--- monty_hall_strategy_test.py
+++ monty_hall_strategy_test.py
@@ -1,18 +1,26 @@
 from monty_hall.game import Game
 
 
+game_count = 1000
 print("Testing 'choose the same door' strategy")
 won_games = 0
-for i in range(1000):
+for i in range(game_count):
     game = Game()
     game.first_guess(1)
     game_won = game.final_guess(1)
     won_games += game_won
 
-print(f"Win rate = {won_games}/1000")
+print(f"Win rate = {won_games}/{game_count}")
 
 
 print("Testing 'choose the other door' strategy")
 won_games = 0
-for i in range(1000):
-    # ...???
+for i in range(game_count):
+    game = Game()
+    empty_door = game.first_guess(1)
+    doors = list(range(1, game.door_count + 1))
+    doors.remove(1)
+    doors.remove(empty_door)
+    game_won = game.final_guess(doors[0])
+    won_games += game_won
+print(f"Win rate = {won_games}/{game_count}")
